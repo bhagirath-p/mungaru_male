5a8b1e3c91424c3cb00903ad4dc065a1cfd6221a
Shashank Harinath <Shank@Shashanks-MacBook-Pro.local>
2015-12-20 02:13:06 +0530

    Model Midway

--- Weather.R
+++ Weather.R
@@ -100,7 +100,13 @@ train <- tr[,.(
 
 train <- data.frame(train)
 train <- subset(train, select=-c(1))
-train <- scale(train)
+
+train_mean <- apply(train, 2, mean)
+train_sd <- apply(train, 2, sd)
+
+train <- scale(train, scale=TRUE)
+colMeans(train)
+apply(train, 2, sd)
 
 h2o.init(nthreads=-1, max_mem_size = '1g')
 trainHex <- as.h2o(train, destination_frame="train.hex")
@@ -118,4 +124,102 @@ target <- "windDegMean"
 
 rfHex<-h2o.randomForest(x=cols, y=target, training_frame=trainHex, model_id="timeModel.hex", ntrees=1700, sample_rate = 0.6, verbose = TRUE)
 
-predictions<-as.data.frame(h2o.predict(rfHex,testHex))
+# Collect data
+testSfo = getWeatherForDate("SFO", "2013-01-01", end_date="2013-01-25", 
+                        opt_detailed=T,
+                        opt_custom_columns=T, custom_columns=c(1,2,3,4,5,6,7,8,9,10,11,12,13,14))
+
+# Format Date
+testSfo$DateUTC <- format(strptime(testSfo$DateUTC, "%Y-%m-%d"), "%Y-%m-%d")
+
+cols <- c("TemperatureC", "Dew_PointC", "Humidity", "Sea_Level_PressurehPa", "VisibilityKm", "Wind_Direction", "Wind_SpeedKm_h", "Gust_SpeedKm_h", "Precipitationmm", "WindDirDegrees", "DateUTC")
+te <- testSfo[, cols]
+
+te <- data.table(te)
+
+te$TemperatureC[which(te$TemperatureC == "N/A")] <- 0
+te$TemperatureC <- as.double(te$TemperatureC)
+
+te$Dew_PointC[which(te$Dew_PointC == "N/A")] <- 0
+te$Dew_PointC <- as.double(te$Dew_PointC)
+
+te$Humidity <- as.double(te$Humidity)
+#te$Humidity[is.na(te$Humidity)] <- 0
+
+# Categorical
+#te$Wind_Direction[which(te$Wind_Direction == "Calm")] <- 4
+#te$Wind_Direction[is.na(te$Wind_Direction)] <- 0
+#te$Wind_Direction <- strtoi(te$Wind_Direction)
+
+te$Wind_SpeedKm_h[which(te$Wind_SpeedKm_h == "Calm")] <- 4
+te$Wind_SpeedKm_h <- as.double(te$Wind_SpeedKm_h)
+
+te$Gust_SpeedKm_h[is.na(te$Gust_SpeedKm_h)] <- 0
+te$Gust_SpeedKm_h[which(te$Gust_SpeedKm_h == "-")] <- 0
+te$Gust_SpeedKm_h <- as.double(te$Gust_SpeedKm_h)
+
+te$Precipitationmm[which(te$Precipitationmm == "N/A")] <- 0
+te$Precipitationmm <- as.double(te$Precipitationmm)
+
+test <- te[,.(
+  temp = sum(TemperatureC, na.rm=T),
+  tempMean = mean(TemperatureC, na.rm=T),
+  tempSd = sd(TemperatureC, na.rm=T),
+  tempMin = min(TemperatureC, na.rm=T),
+  tempMax = max(TemperatureC, na.rm=T),
+  
+  dewPoint = sum(Dew_PointC, na.rm=T),
+  dewPointMean = mean(Dew_PointC, na.rm=T),
+  dewPointSd = sd(Dew_PointC, na.rm=T),
+  dewPointMin = min(Dew_PointC, na.rm=T),
+  dewPointMax = max(Dew_PointC, na.rm=T),
+  
+  humidity = sum(Humidity, na.rm=T),
+  humidityMean = mean(Humidity, na.rm=T),
+  humiditySd = sd(Humidity, na.rm=T),
+  humidityMin = min(Humidity, na.rm=T),
+  humidityMax = max(Humidity, na.rm=T),
+  
+  seaLevelPressure = mean(Sea_Level_PressurehPa, na.rm=T),
+  
+  visibility = sum(VisibilityKm, na.rm=T),
+  visibilityMean = mean(VisibilityKm, na.rm=T),
+  visibilitySd = sd(VisibilityKm, na.rm=T),
+  visibilityMin = min(VisibilityKm, na.rm=T),
+  visibilityMax = max(VisibilityKm, na.rm=T),
+  
+  #windDirection = mean(Wind_Direction, na.rm=T),
+  
+  windSpeed = sum(Wind_SpeedKm_h, na.rm=T),
+  windSpeedMean = mean(Wind_SpeedKm_h, na.rm=T),
+  windSpeedSd = sd(Wind_SpeedKm_h, na.rm=T),
+  windSpeedMin = min(Wind_SpeedKm_h, na.rm=T),
+  windSpeedMax = max(Wind_SpeedKm_h, na.rm=T),
+  
+  gustSpeed = sum(Gust_SpeedKm_h, na.rm=T),
+  gustSpeedMean = mean(Gust_SpeedKm_h, na.rm=T),
+  gustSpeedSd = sd(Gust_SpeedKm_h, na.rm=T),
+  gustSpeedMin = min(Gust_SpeedKm_h, na.rm=T),
+  gustSpeedMax = max(Gust_SpeedKm_h, na.rm=T),
+  
+  percipitation = sum(Precipitationmm, na.rm=T),
+  percipitationMean = mean(Precipitationmm, na.rm=T),
+  percipitationSd = sd(Precipitationmm, na.rm=T),
+  percipitationMin = min(Precipitationmm, na.rm=T),
+  percipitationMax = max(Precipitationmm, na.rm=T),
+  
+  windDeg = sum(WindDirDegrees, na.rm=T),
+  windDegMean = mean(WindDirDegrees, na.rm=T),
+  windDegSd = sd(WindDirDegrees, na.rm=T),
+  windDegMin = min(WindDirDegrees, na.rm=T),
+  windDegMax = max(WindDirDegrees, na.rm=T)
+), DateUTC]
+
+test <- data.frame(test)
+test <- subset(test, select=-c(1))
+
+test <- read.csv('/Users/Shank/Downloads/final-1.csv')
+testHex<-as.h2o(test, destination_frame="test.hex")
+
+predictions<-as.data.frame(h2o.predict(rfHex, testHex))
+
