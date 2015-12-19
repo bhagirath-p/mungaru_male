44324ce08c2a7e98e1cb0583847a358ade06e9b9
Shashank Harinath <Shank@Shashanks-MacBook-Pro.local>
2015-12-19 17:38:24 +0530

    Data Preprocessing

--- Weather.R
+++ Weather.R
@@ -1,6 +1,7 @@
 library(weatherData)
 library(data.table)
 library(plyr)
+library(AnomalyDetection)
 
 # Collect data
 sfo = getWeatherForDate("SFO", "2014-01-01", end_date="2015-01-02", 
@@ -20,32 +21,20 @@ tr <- data.table(tr)
 tr$TemperatureC[which(tr$TemperatureC == "N/A")] <- 0
 tr$Dew_PointC[which(tr$Dew_PointC == "N/A")] <- 0
 
-tr$Humidity[is.na(tr$Humidity)] <- 0
-tr$Humidity <- strtoi(tr$Humidity)
+# Categorical
+#tr$Wind_Direction[which(tr$Wind_Direction == "Calm")] <- 4
+#tr$Wind_Direction[is.na(tr$Wind_Direction)] <- 0
+#tr$Wind_Direction <- strtoi(tr$Wind_Direction)
 
-tr$Sea_Level_PressurehPa[which(tr$Sea_Level_PressurehPa == "N/A")] <- 0
-tr$VisibilityKm[which(tr$VisibilityKm == "N/A")] <- 0
-tr$VisibilityKm[which(tr$VisibilityKm < 0)] <- 0
-
-tr$Wind_Direction[which(tr$Wind_Direction == "Calm")] <- 4
-tr$Wind_Direction[is.na(tr$Wind_Direction)] <- 0
-tr$Wind_Direction <- strtoi(tr$Wind_Direction)
-
-tr$Wind_SpeedKm_h[is.na(tr$Wind_SpeedKm_h)] <- 0
-tr$Wind_SpeedKm_h <- strtoi(tr$Wind_SpeedKm_h)
+tr$Wind_SpeedKm_h[which(tr$Wind_SpeedKm_h == "Calm")] <- 4
+tr$Wind_SpeedKm_h <- as.double(tr$Wind_SpeedKm_h)
 
 tr$Gust_SpeedKm_h[is.na(tr$Gust_SpeedKm_h)] <- 0
 tr$Gust_SpeedKm_h[which(tr$Gust_SpeedKm_h == "-")] <- 0
-tr$Gust_SpeedKm_h <- strtoi(tr$Gust_SpeedKm_h)
+tr$Gust_SpeedKm_h <- as.double(tr$Gust_SpeedKm_h)
 
 tr$Precipitationmm[which(tr$Precipitationmm == "N/A")] <- 0
-tr$Precipitationmm <- strtoi(tr$Precipitationmm)
-
-#tr$Events[which(tr$Events == "N/A")] <- ""
-#tr$Conditions[which(tr$Conditions == "N/A")] <- ""
-
-tr$WindDirDegrees[which(tr$WindDirDegrees == "N/A")] <- 0
-tr$WindDirDegrees <- strtoi(tr$WindDirDegrees)
+tr$Precipitationmm <- as.double(tr$Precipitationmm)
 
 train <- tr[,.(
   temp = mean(TemperatureC, na.rm=T),
@@ -53,7 +42,7 @@ train <- tr[,.(
   humidity = mean(Humidity, na.rm=T),
   seaLevelPressure = mean(Sea_Level_PressurehPa, na.rm=T),
   visibility = mean(VisibilityKm, na.rm=T),
-  windDirection = mean(Wind_Direction, na.rm=T),
+  #windDirection = mean(Wind_Direction, na.rm=T),
   windSpeed = mean(Wind_SpeedKm_h, na.rm=T),
   gustSpeed = mean(Gust_SpeedKm_h, na.rm=T),
   percipitation = mean(Precipitationmm, na.rm=T),
